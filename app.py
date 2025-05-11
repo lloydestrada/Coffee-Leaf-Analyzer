27a3dc2d272be23a7fd3aba5601d8a1549a69acc
Aries Salvador <142958841+ariessalvador@users.noreply.github.com>
2025-05-11 21:38:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -176,8 +176,7 @@ def process_image():
     return send_file(img_byte_arr, mimetype='image/png')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=true)
 
 
 
